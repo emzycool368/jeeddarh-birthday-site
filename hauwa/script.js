ab67024b96154af183c2285e6fd9d17480bc33ac
emzycool368 <fadumilaemmanuel584@gmail.com>
2026-09-10 19:16:03 +0100

    Delete hauwa/script.js

--- hauwa/script.js
+++
@@ -1,6 +0,0 @@
-const beginBtn = document.getElementById('beginBtn');
-
-beginBtn.addEventListener('click', () => {
-  // Next step: this will transition to the card deck screen
-  console.log('Begin tapped — card deck goes here next');
-});
